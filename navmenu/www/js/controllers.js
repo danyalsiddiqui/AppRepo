06244ddd5660c2da23886f46d7b1a3a78521284f
Danyal Siddiqui <dsiddiqui@ssilhr.com.pk>
2017-01-16 18:44:03 +0500

    Latest code till 1/16/2017

--- navmenu/www/js/controllers.js
+++ navmenu/www/js/controllers.js
@@ -54,37 +54,65 @@ angular.module('starter.controllers', [])
 
 .controller('PlaylistCtrl', function($scope, $stateParams,$rootScope) {
 })
-  .controller('ItemListCtrl',function ($http,$scope,$rootScope) {
+  .controller('ItemListCtrl',function ($http,$scope,$rootScope,$ionicLoading,$ionicPopup) {
+    $rootScope.CenimaSelected='ALL';
+    $rootScope.show = function() {
+      $ionicLoading.show({
+        showBackdrop: true,
+        //noBackdrop: true,
+        template: '<p class="item-icon-left">Loading ...<ion-spinner icon="lines"  class="spinner-positive"/></p>'
+      });
+    };
+    $rootScope.hide = function(){
+      $ionicLoading.hide();
+    };
+
     $rootScope.CurrentDate = {
       value: new Date()
     };
-    $http.jsonp('https://moviestime.herokuapp.com/getMovies?callback=JSON_CALLBACK')
-      .success(function (result) {
-        result.push(JSON.parse('{"movieName":"ALL"}'));
-        result.reverse();
-        $scope.movies = result;
-        $scope.selected = $scope.movies[0];
-      })
-      .error(function (data, status, headers, config) {
-        //this always gets called
-        console.log(status);
-      });
 
-    $http.jsonp('https://moviestime.herokuapp.com/getCenima?callback=JSON_CALLBACK')
-      .success(function (result) {
-        result.push(JSON.parse('{"cenima":"ALL"}'));
-        result.reverse();
-        $rootScope.cenima = result;
-        $rootScope.CenimaSelected = $rootScope.cenima[0];
-      })
-      .error(function (data, status, headers, config) {
-        //this always gets called
-        console.log(status);
-      });
+    $scope.getMovies=function () {
+      $rootScope.show($ionicLoading);
+      $http.jsonp('https://moviestime.herokuapp.com/getMovies?callback=JSON_CALLBACK')
+        .success(function (result) {
+          result.push(JSON.parse('{"movieName":"ALL"}'));
+          result.reverse();
+          $rootScope.movies = result;
+          $rootScope.selected = $rootScope.movies[0];
+        })
+        .error(function (data, status, headers, config) {
+          //this always gets called
+          $ionicPopup.alert({
+            title: 'Failed To fetch Data!',
+            template: 'Kindly restart the app.'
+          });
+        })
+        .finally(function($ionicLoading) {
+          // On both cases hide the loading
+          $rootScope.hide($ionicLoading);
+        });
+    }
+    $scope.getMovies($http);
 
-
+    $scope.filterBySelectValue=function(movieNameSelect){
+      $rootScope.show($ionicLoading);
+      debugger;
+      var url='https://moviestime.herokuapp.com/filterByMovieName?movie='+movieNameSelect.movieName+'&cinema='+$rootScope.CenimaSelected+'&date='+$rootScope.CurrentDate.value+'&callback=JSON_CALLBACK';
+      $http.jsonp(url)
+        .success(function (result) {
+          $rootScope.items = result;
+        })
+        .error(function (data, status, headers, config) {
+          //this always gets called
+          console.log(status);
+        })
+        .finally(function($ionicLoading) {
+          // On both cases hide the loading
+          $rootScope.hide($ionicLoading);
+        });
+    }
     $scope.refresh = function() {
-
+      $scope.show($ionicLoading);
       $http.jsonp('https://moviestime.herokuapp.com/listData?callback=JSON_CALLBACK')
         .success(function (result) {
           $rootScope.items = result;
@@ -93,9 +121,17 @@ angular.module('starter.controllers', [])
         .error(function (data, status, headers, config) {
           //this always gets called
           console.log(status);
+        })
+        .finally(function($ionicLoading) {
+          // On both cases hide the loading
+          $rootScope.hide($ionicLoading);
         });
     }
   })
+
+
+  /////////////////////////////////////************Item Controller***********/////////////////////////////////////////////////
+
   .controller('ItemCtrl',function ($scope,$stateParams,$rootScope,$sce) {
 
 
@@ -111,12 +147,40 @@ var id=$stateParams.itemId;
     }
   })
 
-  .controller('PopupCrtl',function ($scope,$stateParams,$rootScope,$ionicPopup,$http) {
-    $scope.filterPopup= function(){
+
+
+  /////////////////////////////////////************Pop Up Controller***********/////////////////////////////////////////////////
+
+
+  .controller('PopupCrtl',function ($scope,$stateParams,$rootScope,$ionicPopup,$http,$ionicLoading) {
+
+
+    $scope.filterPopup=function(){
+      $rootScope.show($ionicLoading);
+      $http.jsonp('https://moviestime.herokuapp.com/getCenima?callback=JSON_CALLBACK')
+        .success(function (result) {
+          result.push(JSON.parse('{"cenima":"ALL"}'));
+          result.reverse();
+          $rootScope.cenima = result;
+          $rootScope.CenimaSelected = $rootScope.cenima[0];
+          $scope.openPopup($ionicPopup);
+        })
+        .error(function (data, status, headers, config) {
+          //this always gets called
+          console.log(status);
+        })
+        .finally(function($ionicLoading) {
+          // On both cases hide the loading
+          $rootScope.hide($ionicLoading);
+        });
+    }
+
+
+    $scope.openPopup= function(){
       var confirmPopup = $ionicPopup.confirm({
         title: 'SEARCH BY CHOICE',
         template: ' <label class=" col col-100"  > <b> Select Cenima</b> </label>'+
-        ' <select ng-options="cenima as cenima.cenima for cenima in cenima track by cenima.cenima" class=" col col-100" ng-model="CenimaSelected"></select>'+
+        ' <select id="cenimaPopUp" ng-options="cenima as cenima.cenima for cenima in cenima track by cenima.cenima" class=" col col-100" ng-model="CenimaSelected"></select>'+
         ' <label class=" col col-100"  > <b> Select Date</b> </label>'+
         '     <input type="date"  name="input" ng-model="CurrentDate.value" placeholder="yyyy-MM-dd" min="CurrentDate.value"  />',
 
@@ -127,6 +191,11 @@ var id=$stateParams.itemId;
           type: 'button-block button-positive',
           scope: null,
           onTap: function(e) {
+            debugger;
+            var e = document.getElementById("cenimaPopUp");
+            var strUser = e.options[e.selectedIndex].value;
+            $scope.selectedCenimaName=strUser;
+            $rootScope.CenimaSelected=$scope.selectedCenimaName;
             $scope.applyfilter();
           }
 
@@ -148,17 +217,60 @@ var id=$stateParams.itemId;
     };
 
     // permissions
-    $scope.applyfilter = function() {
-      debugger;
-      $http.jsonp('https://moviestime.herokuapp.com/filterByCenimaAndDate?movie=FORTRESS MALL - LAHORE&date=2016-12-29&callback=JSON_CALLBACK')
+    $scope.applyfilter = function(cenimaName) {
+      $rootScope.show($ionicLoading);
+      var url='https://moviestime.herokuapp.com/filterByCinemaAndDate?cinema='+$rootScope.CenimaSelected+'&date='+$rootScope.CurrentDate.value+'&callback=JSON_CALLBACK';
+      $http.jsonp(url)
         .success(function (result) {
           $rootScope.items = result;
         })
         .error(function (data, status, headers, config) {
           //this always gets called
           console.log(status);
+        })
+        .finally(function($ionicLoading) {
+          // On both cases hide the loading
+          $rootScope.hide($ionicLoading);
         });
     };
+
+  })
+
+
+  .controller('LocationCtrl', function($scope, $cordovaGeolocation,$rootScope) {
+    $rootScope.GetLocation=function() {
+      debugger;
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+
+      .then(function (position) {
+        var lat  = position.coords.latitude
+        var long = position.coords.longitude
+        console.log(lat + '   ' + long)
+      }, function(err) {
+        console.log(err)
+      });
+
+    var watchOptions = {timeout : 3000, enableHighAccuracy: false};
+    var watch = $cordovaGeolocation.watchPosition(watchOptions);
+
+    watch.then(
+      null,
+
+      function(err) {
+        console.log(err)
+      },
+
+      function(position) {
+        var lat  = position.coords.latitude
+        var long = position.coords.longitude
+        console.log(lat + '' + long)
+      }
+    );
+
+    watch.clearWatch();
+    });
 
   })
 
